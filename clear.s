3953169a1da85f4440c5c6f4d3e994ece70c836a
Scott Fones <scott.fones@gmail.com>
2022-05-17 15:06:01 -0400

    done. no test.

--- clear.s
+++ clear.s
@@ -1,3 +1,5 @@
+.data
+    RCNT: .word 7
 .global _start
 .text
 _start:
@@ -5,29 +7,39 @@ _start:
         mov r0,#1
 	    bl openfb
 	    cmp r0,#0
-	    blt done
-
-        #store the clear color in r6
-        mov r6,#0
+	    blt _sexit
 
         #initialize counter and end points
+        #r0 = row, r1 = column, r2 = color
         mov r0,#0
         mov r1,#0
-        mov r3,#7
+        mov r2,#0
+        ldr r3,=RCNT
+        ldr r3,[r3]
 
-        #for loop from [0,64)
+        #for loop from [0,7]
 loop:   cmp r1,r3
         bgt nline
+        mov r7,r0
+        mov r8,r1
+        mov r2,r6
         bl setPixel
+        mov r0,r7
+        mov r1,r8
+        mov r2,#0
+        ldr r3,=RCNT
+        ldr r3,[r3]
         add r1,r1,#1
+        b loop
 
-        #move down one line of leds or exit if done
+        #move down one line of leds [0,7) or exit if done
 nline:  cmp r0,r3
-        bgt done
+        beq _sexit
         add r0,r0,#1
         mov r1,#0
         b loop
 
 	    #clean exit
-done:   mov r7,#1
+_sexit: bl closefb
+        mov r7,#1
 	    svc #0
